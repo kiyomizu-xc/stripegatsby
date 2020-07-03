d9d8c4798ed0019f35b1e49433018c7bd7898433
kiyomizu xc <xcjp31@gmail.com>
2020-07-04 06:40:31 +0800

    master

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `coffee shop`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `stripe gatsby`,
+    author: `@kiyomizu xc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
